94350aea413b67c3326f51e506102d7463538ff7
EvandroGibicoski <51049058+EvandroGibicoski@users.noreply.github.com>
2020-03-03 21:30:47 -0300

    add news cgallengs

---
+++ empresa.js
@@ -0,0 +1,13 @@
+// Objects / Objetos
+
+const company = {
+    name: "Rocketseat",
+    color: "purple" ,
+    focus: "Programming",
+    address: {
+        street: "Rua Guilherme Gembala",
+        number:260,
+    },
+};
+
+console.log(`A Empresa ${company.name} está localizada na ${company.address.street}, ${company.address.number}.`);
